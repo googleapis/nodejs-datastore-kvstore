2fd18506c5ac8389d9003a17e9ebc4d68dd53cd2
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-04 21:48:30 -0700

    chore: upgrade to es6 (#13)

--- src/index.js
+++ src/index.js
@@ -7,54 +7,55 @@
 
 'use strict';
 
-var invalidKeyError = 'Invalid key. Only a string or number may be used.';
+const invalidKeyError = 'Invalid key. Only a string or number may be used.';
 function isValidKey(key) {
   return typeof key === 'string' || typeof key === 'number';
 }
 
-function KVStore(dataset) {
-  if (!(this instanceof KVStore)) {
-    return new KVStore(dataset);
-  }
-
-  this.dataset = dataset;
-}
-
-KVStore.prototype.delete = function(key, callback) {
-  if (!isValidKey(key)) {
-    throw new Error(invalidKeyError);
-  }
-  key = this.dataset.key(['KeyValue', key]);
-  this.dataset.delete(key, callback);
-};
-
-KVStore.prototype.get = function(key, callback) {
-  if (!isValidKey(key)) {
-    throw new Error(invalidKeyError);
-  }
-  key = this.dataset.key(['KeyValue', key]);
-  this.dataset.get(key, function(err, entity) {
-    if (err) {
-      callback(err);
-      return;
+class KVStore {
+  constructor(dataset) {
+    if (!(this instanceof KVStore)) {
+      return new KVStore(dataset);
     }
-    callback(null, entity && entity.value);
-  });
-};
-
-KVStore.prototype.set = function(key, value, callback) {
-  if (!isValidKey(key)) {
-    throw new Error(invalidKeyError);
+    this.dataset = dataset;
   }
-  this.dataset.save(
-    {
-      key: this.dataset.key(['KeyValue', key]),
-      data: {
-        value: value,
+
+  delete(key, callback) {
+    if (!isValidKey(key)) {
+      throw new Error(invalidKeyError);
+    }
+    key = this.dataset.key(['KeyValue', key]);
+    this.dataset.delete(key, callback);
+  }
+
+  get(key, callback) {
+    if (!isValidKey(key)) {
+      throw new Error(invalidKeyError);
+    }
+    key = this.dataset.key(['KeyValue', key]);
+    this.dataset.get(key, (err, entity) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      callback(null, entity && entity.value);
+    });
+  }
+
+  set(key, value, callback) {
+    if (!isValidKey(key)) {
+      throw new Error(invalidKeyError);
+    }
+    this.dataset.save(
+      {
+        key: this.dataset.key(['KeyValue', key]),
+        data: {
+          value: value,
+        },
       },
-    },
-    callback || function() {}
-  );
-};
+      callback || (() => {})
+    );
+  }
+}
 
 module.exports = KVStore;
