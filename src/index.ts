5105ec4f2f878fbb7a634af9ccd06d6e0c10cc55
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-06 14:55:35 -0700

    chore: improve types, enable noImplicitAny (#15)

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,43 @@
 'use strict';
 
 const invalidKeyError = 'Invalid key. Only a string or number may be used.';
-function isValidKey(key) {
+function isValidKey(key: Key) {
   return typeof key === 'string' || typeof key === 'number';
 }
 
+export type Key = string|number;
+export type DeleteCallback = (err?: Error) => void;
+export type SetCallback = (err?: Error) => void;
+export type GetCallback = (err: Error|null, entity?: Entity) => void;
+
+export interface Entity {
+  // tslint:disable-next-line no-any
+  value: any;
+}
+
+export interface SetRequest {
+  key: Key;
+  // tslint:disable-next-line no-any
+  data: any;
+}
+
+export interface DataSet {
+  key(input: Key[]): Key;
+  delete(key: Key, callback: DeleteCallback): void;
+  get(key: Key, callback: GetCallback): void;
+  save(request: SetRequest, callback: SetCallback): void;
+}
+
 export class KVStore {
-  dataset;
-  constructor(dataset) {
+  dataset: DataSet;
+  constructor(dataset: DataSet) {
     if (!(this instanceof KVStore)) {
       return new KVStore(dataset);
     }
     this.dataset = dataset;
   }
 
-  delete(key, callback) {
+  delete(key: Key, callback: DeleteCallback) {
     if (!isValidKey(key)) {
       throw new Error(invalidKeyError);
     }
@@ -29,7 +52,7 @@ export class KVStore {
     this.dataset.delete(key, callback);
   }
 
-  get(key, callback) {
+  get(key: Key, callback: GetCallback) {
     if (!isValidKey(key)) {
       throw new Error(invalidKeyError);
     }
@@ -43,7 +66,8 @@ export class KVStore {
     });
   }
 
-  set(key, value, callback) {
+  // tslint:disable-next-line no-any
+  set(key: Key, value: any, callback: SetCallback) {
     if (!isValidKey(key)) {
       throw new Error(invalidKeyError);
     }
