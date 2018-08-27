db0d22a66c83b1b9d5b82bbd73e1543f24280049
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-27 14:50:27 -0700

    update synth file

--- synth.py
+++ synth.py
@@ -1,9 +1,7 @@
 import synthtool as s
 import synthtool.gcp as gcp
 import logging
-import subprocess
-
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(package_name="gce-images")
+templates = common_templates.node_library()
 s.copy(templates)
