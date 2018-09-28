82a81c9634b24bf2881c3ea6d28a04b448995d4f
Justin Beckwith <justin.beckwith@gmail.com>
2018-09-28 14:54:03 -0700

    feat: add promise support, samples, and system tests (#38)

--- system-test/system.ts
+++ system-test/system.ts
@@ -5,4 +5,25 @@
  * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
  */
 
-console.warn(`no system tests available 👎`);
+import * as assert from 'assert';
+import {KVStore} from '../src';
+const datastore = require('@google-cloud/datastore')();
+
+const key = 'todos';
+const value = ['eat', 'sleep', 'repeat'];
+const store = new KVStore(datastore);
+
+it('should set values', async () => {
+  await store.set(key, value);
+});
+
+it('should get the same values', async () => {
+  const todos = await store.get(key);
+  assert.deepStrictEqual(todos, value);
+});
+
+it('should remove the data', async () => {
+  await store.delete(key);
+  const data = await store.get(key);
+  assert.strictEqual(data, undefined);
+});
